0f7e23a22f80908f9880de053831188812d48e4c
Anthony Chao <qoosuperman@gmail.com>
2019-11-10 16:49:58 +0800

    生出 seesions controller 跟登入頁面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :missions
-  resources :users
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   root "missions#index"
+
+  match '/signup',  to: 'users#new', via: :get
+  match '/signin',  to: 'sessions#new', via: :get
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
