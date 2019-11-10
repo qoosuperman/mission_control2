222dc28f995935c6f63f5428e37224041e5ab911
Anthony Chao <qoosuperman@gmail.com>
2019-11-10 16:17:01 +0800

    實作 註冊的 view 跟 controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(clean_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def clean_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
