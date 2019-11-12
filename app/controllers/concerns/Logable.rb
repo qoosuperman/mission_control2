31a40d65cf44c8b305cebe4a64c9196b8e7ac594
Anthony Chao <qoosuperman@gmail.com>
2019-11-12 08:49:08 +0800

    try fix CI

--- app/controllers/concerns/Logable.rb
+++ app/controllers/concerns/Logable.rb
@@ -1,29 +1,31 @@
-module Logable
-  extend ActiveSupport::Concern
-  # included do  
-  # end
-  # module ClassMethods 
-  # end
+class ApllicationController
+  module Logable
+    extend ActiveSupport::Concern
+    # included do  
+    # end
+    # module ClassMethods 
+    # end
 
-  def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token 
-    self.current_user = user
-  end
+    def sign_in(user)
+      cookies.permanent[:remember_token] = user.remember_token 
+      self.current_user = user
+    end
 
-  def signed_in? 
-    !current_user.nil?
-  end
+    def signed_in? 
+      !current_user.nil?
+    end
 
-  def current_user=(user)
-    @current_user = user 
-  end
-  
-  def current_user
-    @current_user ||= User.find_by(remember_token: cookies[:remember_token])
-  end  
+    def current_user=(user)
+      @current_user = user 
+    end
+    
+    def current_user
+      @current_user ||= User.find_by(remember_token: cookies[:remember_token])
+    end  
 
-  def sign_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
+    def sign_out
+      self.current_user = nil
+      cookies.delete(:remember_token)
+    end
   end
 end
