80cf3288a33ebba296ba27cd85ab31bc326110b6
Anthony Chao <qoosuperman@gmail.com>
2019-12-02 16:26:36 +0800

    fix rspec

--- spec/factories/missions.rb
+++ spec/factories/missions.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     priority { %w(urgent common low ).sample }
     category { %w(company home ).sample }
     start_time { Time.now }
+    end_time { Time.now + 12.hours }
   end
 end
